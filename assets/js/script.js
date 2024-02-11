9bbcbaa352adcb618943aad32928aff04d677467
Jhonatan Gonçalves <jhonatangoncalves2002@gmail.com>
2024-02-10 23:43:50 -0300

    :sparkles: feat: Criação de Janelas Modais com HTML e CSS e melhoria da implementação JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,99 +1,108 @@
-var btnCriptografa = document.getElementById("btn-criptografa");
-var btnDescriptografa = document.getElementById("btn-descriptografa");
+const btnCriptografa = document.querySelector("#btn-criptografa");
+const btnDescriptografa = document.querySelector("#btn-descriptografa");
+const txt = document.querySelector("#txt-de-cript");
+const containerAlerta = document.querySelector('.container__alerta');
+const alerta = document.querySelector('.alerta-requisitos');
+const btnFecharAlerta = document.querySelector('.alerta-requisitos__fechar');
+const txtLateralImg = document.querySelector('.decodificador__txtLateral__img');
+const txtLateralTitulo = document.querySelector('.decodificador__txtLateral__Titulo');
+const txtLateralTxt = document.querySelector('.decodificador__txtLateral__Txt');
+const spanCopiado = document.querySelector('.decodificador__txtLateral__spanCopiado');
+const btnCopiar = document.querySelector('.decodificador__txtLateral__btnCopiar');
 
-var txt = document.getElementById("txt-de-cript");
-
-var decodificador__txtLateral__img = document.getElementsByClassName("decodificador__txtLateral__img")[0];
-var decodificador__txtLateral__Titulo = document.getElementsByClassName("decodificador__txtLateral__Titulo")[0];
-var decodificador__txtLateral__Txt = document.getElementsByClassName("decodificador__txtLateral__Txt")[0];
-
-var spanCopiado = document.getElementsByClassName("decodificador__txtLateral__spanCopiado")[0];
-var btnCopiar = document.getElementsByClassName("decodificador__txtLateral__btnCopiar")[0];
-
-
-
-btnCriptografa.addEventListener("click", function() {
-  var regex = /^[A-Z\s]+$/;
-  var texto = txt.value.trim(); 
-  if (!texto || !regex.test(texto)) {
-     
-      alert("Por favor, insira apenas letras maiúsculas e sem acento.");
-      return; 
-  }
-
-  decodificador__txtLateral__Titulo.style.display = "none";
-  decodificador__txtLateral__img.style.display = "none";
-  decodificador__txtLateral__Txt.innerHTML = criptografar(texto);
-  decodificador__txtLateral__Txt.style.wordWrap = "break-word";
-  btnCopiar.style.display = "block";
+containerAlerta.addEventListener("click", () => {
+  containerAlerta.classList.add("containerDisable");
+  alerta.classList.remove('mostrando');
 });
 
-btnDescriptografa.addEventListener("click", function() {
-  decodificador__txtLateral__Txt.innerHTML = descriptografa(descriptografa(txt.value));
-  btnCopiar.style.display = "block";
+btnFecharAlerta.addEventListener("click", () => {
+  alerta.classList.remove('mostrando');
+  containerAlerta.classList.add("containerDisable");
 });
 
-
-function criptografar(text){
-  let encryptedText = "";
-  for(let i = 0; i < text.length; i++){
-    if(text[i] === "E"){
-      encryptedText += "enter";
-    } else if(text[i] === "I"){
-      encryptedText += "imes";
-    } else if(text[i] === "A"){
-      encryptedText += "ai";
-    } else if(text[i] === "O"){
-      encryptedText += "ober";
-    } else if(text[i] === "U"){
-      encryptedText += "ufat";
-    } else {
-      encryptedText += text[i]; // Mantém caracteres não modificados
-    }
+btnCriptografa.addEventListener("click", () => {
+  const regex = /^[A-Z\s]+$/;
+  const texto = txt.value.trim(); 
+  if (!texto || !regex.test(texto)) {   
+    mostrarAlerta();
+    containerAlerta.classList.remove("containerDisable"); 
+  } else {
+    esconderElementos();
+    txtLateralTxt.innerHTML = criptografar(texto);
+    btnCopiar.style.display = "block";
   }
-  return encryptedText.toUpperCase();
+});
+
+btnDescriptografa.addEventListener("click", () => {
+  const regex = /^[A-Z\s]+$/;
+  const texto = txt.value.trim(); 
+  if (!texto || !regex.test(texto)) {   
+    mostrarAlerta(); 
+  } else {
+    esconderElementos();
+    txtLateralTxt.innerHTML = descriptografar(texto.toUpperCase());
+    btnCopiar.style.display = "block";
+  }
+});
+
+function mostrarAlerta(){
+  alerta.classList.add('mostrando');
+  containerAlerta.classList.remove("containerDisable");  
 }
 
-function descriptografa(text){
+function esconderElementos() {
+  txtLateralTitulo.style.display = "none";
+  txtLateralImg.style.display = "none";
+  txtLateralTxt.style.wordWrap = "break-word";
+}
+
+function criptografar(text){
+  const encryptionKey = {
+    'E': 'enter',
+    'I': 'imes',
+    'A': 'ai',
+    'O': 'ober',
+    'U': 'ufat'
+  };
+
+  return text.split('').map(char => encryptionKey[char] || char).join('').toUpperCase();
+}
+
+function descriptografar(text) {
   const decryptionKey = {
     'ENTER': 'E',
     'IMES': 'I',
     'AI': 'A',
     'OBER': 'O',
     'UFAT': 'U'
-};
+  };
 
-let decrypted = '';
-let i = 0;
-while (i < text.length) {
+  let decrypted = '';
+  let i = 0;
+  while (i < text.length) {
     let found = false;
     for (let key in decryptionKey) {
-        if (text.startsWith(key, i)) {
-            decrypted += decryptionKey[key];
-            i += key.length;
-            found = true;
-            break;
-        }
+      if (text.startsWith(key, i)) {
+        decrypted += decryptionKey[key];
+        i += key.length;
+        found = true;
+        break;
+      }
     }
     if (!found) {
-        decrypted += text[i];
-        i++;
+      decrypted += text[i];
+      i++;
     }
-}
-return  decrypted.toUpperCase();
+  }
+  return decrypted.toUpperCase();
 }
 
-btnCopiar.addEventListener("click", function() {
-  var elementoParaCopiar = decodificador__txtLateral__Txt;
-  var tempInput = document.createElement("textarea");
-  tempInput.value = elementoParaCopiar.textContent || elementoParaCopiar.innerText;
-  document.body.appendChild(tempInput);
- 
-  tempInput.select();
-  document.execCommand("copy");
-  document.body.removeChild(tempInput);
- spanCopiado.style.display = "block";
-  setTimeout(function() {
+
+btnCopiar.addEventListener("click", () => {
+  const textoParaCopiar = txtLateralTxt.textContent || txtLateralTxt.innerText;
+  navigator.clipboard.writeText(textoParaCopiar);
+  spanCopiado.style.display = "block";
+  setTimeout(() => {
     spanCopiado.style.display = "none";
   }, 1000); 
 });
